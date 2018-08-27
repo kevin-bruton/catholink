92b5e7f0e1eb397e053b0f2df2d378cd378008af
Kevin Bruton <kevin.bruton.serrano@ing.com>
2018-08-27 11:55:30 +0200

    Added mongo connection and init and eslint js standard

--- api/server.js
+++ api/server.js
@@ -1,6 +1,33 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+// const passport = require('passport')
+const dbConfig = require('./config/db')
+
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.get('/', function (req, res) {
+  res.send('hello')
+});
+
+(async () => {
+  const mongoConnection = await dbConfig.connect()
+  const collections = await dbConfig.init(mongoConnection)
+  collections.map(collection => {
+    const collectionName = `db${collection.s.name.charAt(0).toUpperCase()}${collection.s.name.substr(1)}`
+    app.set(collectionName, collection)
+  })
+
+  const PORT = process.env.PORT || 5000
+
+  const server = app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}...`))
+
+  process.on('SIGINT', function () {
+    server.close(function () {
+      mongoConnection.close()
+      console.log('DB connection closed')
+    })
+  })
+})()
