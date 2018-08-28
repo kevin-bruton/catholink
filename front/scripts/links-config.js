22e6e1887dce9df802699d33bdd67587b850b61a
Kevin Bruton <kevin.bruton.serrano@ing.com>
2018-08-28 09:38:50 +0200

    Added route aliases

---
+++ front/scripts/links-config.js
@@ -0,0 +1,8 @@
+module.exports = [
+  { name: '@src', target: 'src' },
+  { name: '@actions', target: 'src/actions' },
+  { name: '@components', target: 'src/components' },
+  { name: '@containers', target: 'src/containers' },
+  { name: '@helpers', target: 'src/helpers' },
+  { name: '@reducers', target: 'src/reducers' }
+]
