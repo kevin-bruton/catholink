6c04f05b201cc45e459c6f751d5fca5401b0e70c
Kevin Bruton <kevin.bruton.serrano@ing.com>
2018-09-01 17:14:26 +0200

    Route aliases mod

--- back/scripts/route-alias-config.js
+++ back/scripts/route-alias-config.js
@@ -1,6 +1,7 @@
 module.exports = [
-  { name: '~', target: '.' },
+  { name: '@', target: '.' },
   { name: '@routers', target: 'routers' },
   { name: '@config', target: 'config' },
-  { name: '@auth', target: 'auth' }
+  { name: '@auth', target: 'auth' },
+  { name: '@db', target: 'db' }
 ]
