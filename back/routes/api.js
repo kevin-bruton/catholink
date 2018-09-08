0360635c9c8930c4e741db01ff164ecbf8cc029c
Kevin Bruton <kevin.bruton.serrano@ing.com>
2018-09-08 22:55:42 +0200

    Api authorization added

--- back/routes/api.js
+++ back/routes/api.js
@@ -1,5 +1,9 @@
+const jwt = require('jsonwebtoken')
+const jwtSecret = require('@auth/jwt-secret')
 const express = require('express')
 const router = express.Router()
+
+router.use(authorizeApi)
 
 /* GET users listing. */
 router.get('/', (req, res, next) => {
@@ -12,3 +16,19 @@ router.get('/user', (req, res, next) => {
 })
 
 module.exports = router
+
+async function authorizeApi (req, res, next) {
+  const bearer = req.get('Authorization')
+  if (bearer) {
+    const token = bearer.slice('Bearer '.length)
+    if (token) {
+      try {
+        jwt.verify(token, jwtSecret)
+        next()
+      } catch (err) {
+        res.status(401).send({ error: 'Unauthorized' })
+      }
+    }
+  }
+  res.status(401).send({ error: 'Unauthorized' })
+}
--- back/routes/auth.js
+++ back/routes/auth.js
@@ -6,12 +6,22 @@ const db = require('@db')
 
 router.post('/', async (req, res, next) => {
   console.log(`Authenticating user with email: ${req.body.email}; password: ${req.body.password}`)
-  const found = await (await db.users.find({ email: req.body.email, password: req.body.password })).toArray()
-  if (req.body.email === found[0].email && req.body.password === found[0].password) {
-    console.log(' -> Authenticated!\n')
-    return res.json({user: found[0], token: jwt.sign(found[0], jwtSecret)})
+  let found
+  try {
+    const cursor = await db.users.find({ email: req.body.email, password: req.body.password })
+    found = await cursor.toArray()
+    console.log(found)
+  } catch (err) {
+    console.log(`ERROR: ${err}`)
+    return res.json({ error: 'Server error' })
   }
-  return res.json({ error: 'Invalid credentials' })
+  if (found.length) {
+    if (req.body.email === found[0].email && req.body.password === found[0].password) {
+      console.log(' -> Authenticated!\n')
+      return res.json({user: found[0], token: jwt.sign(found[0], jwtSecret)})
+    }
+  }
+  return res.status(401).json({ error: 'Invalid credentials' })
 })
 
 module.exports = router
