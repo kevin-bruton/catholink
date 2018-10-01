1eba27b7b53f2234abab15f928b7c87271ba1fb9
Kevin Bruton <kevin.bruton.serrano@ing.com>
2018-10-01 22:25:56 +0200

    Display gospel on home page

--- front/src/components/Home/index.js
+++ front/src/components/Home/index.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import { post } from '@services'
+import { usersLanguage } from '@helpers/usersLanguage'
+import * as appState from '@state'
 
-export const Home = () => {
-  return (
-    <div>
-      <h2>Home Page</h2>
-      <p>Content</p>
-    </div>
-  )
+export class Home extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      gospel: {},
+      day: ''
+    }
+  }
+  async componentDidMount () {
+    const todaysDate = (new Date()).toLocaleString('en-AU').slice(0, 10).split('/').reverse().join('-')
+    let gospel = appState.getGospel()
+    if (!gospel.title || !gospel.text) {
+      gospel = await post('gospel', { lang: usersLanguage, date: todaysDate })
+    }
+    appState.setGospel(gospel)
+    this.setState({ gospel })
+  }
+  render () {
+    return (
+      <div>
+        <h1>Home Page</h1>
+        <h1>{this.state.day}</h1>
+        <h1>{this.state.gospel.title}</h1>
+        <small>{this.state.gospel.text}</small>
+      </div>
+    )
+  }
 }
