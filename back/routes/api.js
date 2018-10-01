c4ed7b107ad738682849565bd6588258d56a808d
Kevin Bruton <kevin.bruton.serrano@ing.com>
2018-10-01 22:24:24 +0200

    gospel endpoint added

--- back/routes/api.js
+++ back/routes/api.js
@@ -2,6 +2,8 @@ const jwt = require('jsonwebtoken')
 const privateKey = require('@auth/jwt-secret').privateKey
 const express = require('express')
 const router = express.Router()
+const { getRequest } = require('@request')
+const { getGospel, setGospel } = require('@gospel')
 
 router.use(authorizeApi)
 
@@ -12,6 +14,21 @@ router.get('/', (req, res, next) => {
 
 router.get('/user', (req, res, next) => {
   res.send()
+})
+
+router.post('/gospel', async (req, res) => {
+  // can be optimized with a cronjob to get gospel only once a day for supported languages
+  const lang = ((langCode) => { return { en: 'AM', es: 'SP' }[langCode] })(req.body.lang)
+  let gospel = getGospel()
+  if (!gospel.text) {
+    console.log('Gospel not found. Fetching...')
+    const data = await getRequest(`https://publication.evangelizo.ws/${lang}/days/${req.body.date}`)
+    const gospelIdx = data.data.readings.length - 1
+    gospel = { text: (data.data || {}).readings[gospelIdx].text, title: data.data.readings[gospelIdx].title }
+    console.log('Going to set gospel')
+    setGospel(gospel)
+  } else console.log('Gospel found. Dont have to fetch')
+  res.send(gospel)
 })
 
 module.exports = router
