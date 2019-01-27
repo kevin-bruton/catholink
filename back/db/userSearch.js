5f1a45c9c2b63fd2590f39a29857241cdf084d15
Kevin Bruton <kevin.bruton.serrano@ing.com>
2019-01-27 12:36:25 +0100

    Extended user search, not only to firstName but to surname too

--- back/db/userSearch.js
+++ back/db/userSearch.js
@@ -2,14 +2,22 @@ const db = require('@db')
 
 const userSearch = async searchText => {
   let found
-  console.log('Searching db with text:', searchText)
   try {
-    found = await (await db.users().find({"firstName" : {$regex: new RegExp(searchText)}}).project({_id: 0, firstName: 1, surname: 1})).toArray()
+    found = await (await db.users()
+      .find(
+        {$or:
+          [
+            {"firstName" : {$regex: new RegExp(searchText)}},
+            {"surname": {$regex: new RegExp(searchText)}}
+          ]
+        }
+      )
+      .project({_id: 0, firstName: 1, surname: 1}))
+      .toArray()
   } catch (err) {
     console.log('ERROR trying to find searchText db.users().find:', err)
     return {error: 'DB failure'}
   }
-  console.log(found)
   return found
 }
 
