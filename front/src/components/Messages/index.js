31d41b34d1a80eaeae7491bc6151a1d89442d126
Kevin Bruton <kevin.jose.bs@gmail.com>
2019-04-21 11:55:44 +0200

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit b94ea6253961f44ab3710c6906a8857ae34b3e0b.

--- front/src/components/Messages/index.js
+++ front/src/components/Messages/index.js
@@ -4,7 +4,6 @@ import { getStoreValue, storeCategory, subscribeStoreChanges, sendStoreEvent, ev
 import { get as getRequest } from '@services/request'
 import styles from './styles.scss'
 import literals from './literals'
-import '../MyLit'
 
 export class Messages extends Component {
   constructor (props) {
--- front/src/components/MyLit/index.js
+++
@@ -1,9 +0,0 @@
-import {LitElement, html} from 'lit-element'
-
-class MyLit extends LitElement {
-    render () {
-        return html`<p>A paragraph</p>`
-    }
-}
-
-customElements.define('my-lit', LitElement)
