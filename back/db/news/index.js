89d0de9b4d7fb063d17365d09f323e8b0da610a8
Kevin Bruton <kevin.bruton@gft.com>
2019-11-10 11:37:23 +0100

    News DB: postNews getNews

--- back/db/news/index.js
+++ back/db/news/index.js
@@ -1,11 +1,12 @@
 module.exports = {
-  addPost
+  postNewsDB,
+  getNewsDB
 }
 
 const db = require('@db/')
 const log = require('@log/')
 
-async function addPost (post) {
+async function postNewsDB (post) {
   try {
     log('Insert new post in news collection')
     log(post)
@@ -16,5 +17,17 @@ async function addPost (post) {
     log(`DB ERROR trying to insert a new post.`)
     log(err)
     return {error: 'DB failure'}
+  }
+}
+
+async function getNewsDB (profileId, page) {
+  try {
+    log('Getting news for user with profileId ' + profileId)
+    const resp = await db.news().find({audience: profileId}).project({_id: 0, audience: 0}).sort({timestamp: -1}).skip(25 * (page - 1)).limit(25).toArray()
+    console.log(resp)
+    return resp
+  } catch (err) {
+    log('DB ERROR: trying to get news for user with profileId ' + profileId + ':' + err)
+    return {error: 'DB ERROR'}
   }
 }
